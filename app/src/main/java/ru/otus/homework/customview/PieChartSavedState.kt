bdd01d17abc044c8c40c40a17ee1e8a72194bded
Kamentsev <kamentsev@mkb.ru>
2024-08-28 18:07:10 +0300

    CustomViewPieChart

---
+++ app/src/main/java/ru/otus/homework/customview/PieChartSavedState.kt
@@ -0,0 +1,38 @@
+package ru.otus.homework.customview
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+
+internal class PieChartSavedState : View.BaseSavedState {
+    var centerText: String? = null
+    var sectionList: List<PieChartSection> = emptyList()
+
+    constructor(superState: Parcelable?) : super(superState)
+
+    private constructor(source: Parcel) : super(source) {
+        centerText = source.readString()
+
+        source.readList(sectionList, ClassLoader.getSystemClassLoader())
+    }
+
+    companion object CREATOR : Parcelable.Creator<PieChartSavedState> {
+        override fun createFromParcel(parcel: Parcel): PieChartSavedState {
+            return PieChartSavedState(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PieChartSavedState?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        super.writeToParcel(parcel, flags)
+        parcel.writeString(centerText)
+        parcel.writeTypedList(sectionList)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+}
---
+++ app/src/main/java/ru/otus/homework/customview/PieChartSection.kt
@@ -0,0 +1,29 @@
+package ru.otus.homework.customview
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+
+@Parcelize
+data class PieChartSection(
+    val pieChartItems: List<PieChartItem>,
+    val startAngle: Float,
+    val sweepAngle: Float,
+    val color: Int
+) : Parcelable
+
+@Parcelize
+data class PieChartItem(
+    val name: String,
+    val category: String,
+    val amount: Int
+) : Parcelable
+
+@Serializable
+data class PayloadData(
+    val id: Int,
+    val name: String,
+    val amount: Int,
+    val category: String,
+    val time: Long
+)
